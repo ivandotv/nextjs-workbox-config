577cea9749f219854b061e868b30f2a4162939ff
Ivan Vlatković <390700+ivandotv@users.noreply.github.com>
2022-06-14 22:13:25 +0200

    setup script

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,114 @@
 /**
- * Demo function for template repository
- *
- * @remarks
- * Read more about TSDoc at: {@link https://github.com/microsoft/tsdoc}
- *
- * @returns Nothing!
- *
- * @beta
- */
-export function demo(): void {
-  if (__DEV__) {
-    console.log('this should only log in development build')
+ *  Modified webpack + workbox script
+ *  from: https://github.com/cansin/next-with-workbox
+ * */
+import glob from 'glob'
+import crypto from 'node:crypto'
+import path from 'node:path'
+import WorkboxPlugin from 'workbox-webpack-plugin'
+
+const getRevision = (file: string) =>
+  crypto.createHash('md5').update(Buffer.from(file)).digest('hex')
+
+export function withWorkbox(
+  nextConfig: {
+    webpack?: (...args: any[]) => Record<string, any>
+    workbox?: {
+      additionalManifestEntries?: { url: string; revision: string }[]
+      dest?: string
+      dontCacheBustURLsMatching?: false
+      exclude?: string[]
+      disable?: boolean
+      modifyURLPrefix?: Record<string, string>
+      swDest?: string
+      swSrc?: string
+    }
+  } = {}
+) {
+  return {
+    ...nextConfig,
+    webpack(config: Record<string, any>, options: Record<string, any>) {
+      if (typeof nextConfig.webpack === 'function') {
+        config = nextConfig.webpack(config, options)
+      }
+
+      const wbConfig = {
+        additionalManifestEntries: [],
+        dest: 'public',
+        dontCacheBustURLsMatching: false,
+        exclude: [],
+        disable: false,
+        modifyURLPrefix: {},
+        swDest: 'sw.js',
+        swSrc: false,
+        ...(options.config.workbox || {})
+      }
+
+      if (options.isServer) {
+        return config
+      }
+      if (!wbConfig.swSrc) {
+        throw new Error('service worker path missing')
+      }
+
+      if (wbConfig.disable) {
+        console.log('> Progressive web app  is disabled')
+
+        return config
+      }
+
+      const swDestPath = path.join(options.dir, wbConfig.dest, wbConfig.swDest)
+
+      console.log('> Compiling progressive web app')
+      console.log(`> Service worker destination path: "${swDestPath}"`)
+
+      const defaultDontCacheBustURLsMatching = /^\/_next\/static\/.*/iu
+
+      const defaultWorkboxOptions = {
+        swDest: swDestPath,
+        dontCacheBustURLsMatching: wbConfig.dontCacheBustURLsMatching
+          ? new RegExp(
+              `${wbConfig.dontCacheBustURLsMatching.source}|${defaultDontCacheBustURLsMatching.source}`,
+              'iu'
+            )
+          : defaultDontCacheBustURLsMatching,
+        additionalManifestEntries: glob
+          .sync('**/*', {
+            cwd: wbConfig.dest,
+            nodir: true
+          })
+          .filter((f: string) => f.indexOf(wbConfig.swDest) !== 0)
+          .map((f: string) => ({
+            url: `/${f}`,
+            revision: getRevision(`public/${f}`)
+          }))
+          .concat(wbConfig.additionalManifestEntries),
+        exclude: [
+          /_next\/server\/.*/i,
+          /middleware-.+(js|json)/i,
+          /^build-manifest\.(js|json)$/i,
+          /^react-loadable-manifest\.(js|json)$/i,
+          /\/_error\.js$/i,
+          /\.js\.map$/i,
+          ...wbConfig.exclude
+        ],
+        modifyURLPrefix: {
+          [`${config.output.publicPath || ''}static/`]: '/_next/static/',
+          ...wbConfig.modifyURLPrefix
+        }
+      }
+
+      const swSrcPath = path.join(options.dir, wbConfig.swSrc)
+      console.log(`> Service worker source path: "${swSrcPath}"`)
+      console.log('> Using "WorkboxPlugin.InjectManifest"')
+      config.plugins.push(
+        new WorkboxPlugin.InjectManifest({
+          swSrc: swSrcPath,
+          ...defaultWorkboxOptions
+        })
+      )
+
+      return config
+    }
   }
-  console.log(__VERSION__)
 }
-demo()
